18ec31f6031cf4b17fbf4fb3cb3f7f54f5f70cf6
Bianca <biahdrew94@gmail.com>
2022-02-02 20:28:28 -0300

    adicionando arquivos

---
+++ 01_Fundamentos/04-bloco-introducao-javascript-logica-programacao/01-primeiros-passos/scripts.js
@@ -0,0 +1,25 @@
+//Definindo constantes:
+const a = 40;
+const b = 5;
+
+//Adição
+const adi = a + b ;
+console.log (adi);
+
+//Subtração
+const sub = a - b;
+console.log (sub);
+
+//Multiplicação
+const multi = a * b;
+console.log (multi);
+
+//Divisão
+const div = a / b;
+console.log (div);
+
+//Módulo
+const mod = a % b
+console.log (mod);
+
+
