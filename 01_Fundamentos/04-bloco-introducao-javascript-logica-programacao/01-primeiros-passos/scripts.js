2383cc65afcc15f1a4ebb1a3f193dd9bf22425ab
Bianca <biahdrew94@gmail.com>
2022-02-02 20:56:52 -0300

    questao 3

--- 01_Fundamentos/04-bloco-introducao-javascript-logica-programacao/01-primeiros-passos/scripts.js
+++ 01_Fundamentos/04-bloco-introducao-javascript-logica-programacao/01-primeiros-passos/scripts.js
@@ -1,4 +1,4 @@
-//Definindo constantes:
+/* //Operações aritméticas: Questão 1
 const a = 40;
 const b = 5;
 
@@ -20,6 +20,33 @@ console.log (div);
 
 //Módulo
 const mod = a % b
-console.log (mod);
+console.log (mod); */
 
+//Retorne o maior de dois: Questão 2
+/* const x = 16;
+const y = 37;
 
+if (x > y) {
+    console.log ('X é maior que y.');
+}
+else{
+    console.log ('Y é maior que x.');
+} */
+
+//Retorne o maior de três: Questão 3
+const valor1 = 45;
+const valor2 = 69;
+const valor3 = 58;
+
+if (valor1 > valor2 && valor1 > valor3) {
+    console.log (' valor1 é o maior número dos três');
+}
+else if (valor2 > valor1 && valor2 > valor3) {
+    console.log ('valor2 é o maior número entre os três');
+} 
+else if (valor3 > valor1 && valor3 > valor2) {
+    console.log (' valor3 é o maior número');
+}
+else {
+    console.log ('todos os valores são iguais');
+}
