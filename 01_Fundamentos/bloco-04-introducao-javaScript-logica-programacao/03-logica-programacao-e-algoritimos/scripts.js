8215be45eabc2d1d011df080fa5e58a8fee7bfdc
Bianca <biahdrew94@gmail.com>
2022-02-04 18:39:33 -0300

    questao 2

--- 01_Fundamentos/bloco-04-introducao-javaScript-logica-programacao/03-logica-programacao-e-algoritimos/scripts.js
+++ 01_Fundamentos/bloco-04-introducao-javaScript-logica-programacao/03-logica-programacao-e-algoritimos/scripts.js
@@ -1,8 +1,18 @@
 //exercise.js
-let fatorial = 10;
+//Questao 1
+/* let fatorial = 10;
 let retorno = 1;
 
 for (let index = 1; index < fatorial; index+=1) {
     retorno *= index;
 }
-console.log(retorno);
+console.log(retorno); */
+
+//Questao 2
+let word = 'Banana'.toLowerCase();
+let restruturar = '';
+
+for (let l = 0; l < word.length; l +=1) {
+    restruturar = word[l] + restruturar;
+}
+console.log(restruturar);
