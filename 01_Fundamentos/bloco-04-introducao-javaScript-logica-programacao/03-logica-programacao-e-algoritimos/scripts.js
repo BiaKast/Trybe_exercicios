a015d5aef95dd6ab113bec1ca1336356ea28fa13
Bianca <biahdrew94@gmail.com>
2022-02-04 20:02:18 -0300

    questao 3

--- 01_Fundamentos/bloco-04-introducao-javaScript-logica-programacao/03-logica-programacao-e-algoritimos/scripts.js
+++ 01_Fundamentos/bloco-04-introducao-javaScript-logica-programacao/03-logica-programacao-e-algoritimos/scripts.js
@@ -9,10 +9,27 @@ for (let index = 1; index < fatorial; index+=1) {
 console.log(retorno); */
 
 //Questao 2
-let word = 'Banana'.toLowerCase();
+/* let word = 'Banana' //.toLowerCase(); caso quisesse imprimir minusculo
 let restruturar = '';
 
 for (let l = 0; l < word.length; l +=1) {
     restruturar = word[l] + restruturar;
 }
-console.log(restruturar);
+console.log(restruturar); */
+
+//Questao 3
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+
+let menorPalavra = array[0];
+let maiorPalavra = array[0];
+
+for (let index = 0; index < array.length; index +=1 ){
+    if(array[index].length > maiorPalavra.length){
+        maiorPalavra = array[index];
+    }
+    if(array[index].length < menorPalavra.length){
+        menorPalavra = array[index];
+    }
+}   
+console.log(maiorPalavra);
+console.log(menorPalavra);
