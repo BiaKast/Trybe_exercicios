c558c9f1d73f1b045aa16da78584005f0434356d
Bianca <biahdrew94@gmail.com>
2022-02-07 20:50:51 -0300

    questoes resolvidas ate a 3

--- 01_Fundamentos/bloco-04-introducao-javaScript-logica-programacao/04-objetos-funções/scripts.js
+++ 01_Fundamentos/bloco-04-introducao-javaScript-logica-programacao/04-objetos-funções/scripts.js
@@ -1,51 +1,33 @@
-/* let player = {
-  name: "Marta",
-  lastName: "Silva",
-  age: 34,
-  medals: {
-    golden: 2,
-    silver: 3,
-  },
+let info = {
+  personagem: "Margarida",
+  origem: "Pato Donald",
+  nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
 };
-console.log(
-  "A jogadora " + player.name + " " + player.lastName + " tem " + player.age + ' anos de idade'
-); */
+/* console.log ('Bem-vinda, ' + info.personagem); */
 
-//2
-/* let player = {
-    name: "Marta",
-    lastName: "Silva",
-    age: 34,
-    medals: {
-      golden: 2,
-      silver: 3,
-    },
+info.recorrente = "Sim";
+/* console.log(info); */
+/* for(let key in info){
+    console.log(key);
+  } */
+/* for (let valores in info){
+    console.log(info[valores]);
+  } */
+
+let segundo = {
+  personagem: "Tio Patinhas",
+  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+  nota: "O último MacPatinhas",
+  recorrente: "Sim",
 };
-player. bestInTheWorld = [2006, 2007, 2008, 2009, 2010, 2018]
-player. fullName = 'Marta Silva';
-
-console.log('A jogadora ' + player.fullName + ' foi eleita a melhor do mundo por ' + player.bestInTheWorld.length + ' vezes');
-
-console.log('A jogadora possui ' + player.medals.golden + ' medalhas de ouro e ' + player.medals.silver + ' medalhas de prata.');
- */
-
-/* let names = {
-    person1: 'João',
-    person2: 'Maria',
-    person3: 'Jorge',
-};
-for (let index in names) {
-    console.log('Olá ' + names[index])
-} */ 
-//Olá João
-//Olá Maria
-//Olá Jorge
-
-let car = {
-    model: 'A3 Sedan',
-    manufacturer: 'Audi',
-    year: 2020
-  };
-  for (let keyValor in car) {
-      console.log(keyValor + ': ' + car[keyValor]);
+function infoPersonagem(objeto1, objeto2) {
+ for (let valor in objeto1) {
+     if (objeto1[valor] === 'Sim' && objeto2[valor] === 'Sim'){
+         return console.log ('Ambos recorrentes')
+     }
+    console.log(objeto1[valor] + ' e ' + objeto2[valor])
+    return ''
   }
+  
+}
+console.log(infoPersonagem(info, segundo));
