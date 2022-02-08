48c50847d31b981df3226b1ee048cda68eaa75b2
BiaKast <biahdrew94@gmail.com>
2022-02-07 21:47:50 -0300

    ok

--- 01_Fundamentos/bloco-04-introducao-javaScript-logica-programacao/04-objetos-funções/scripts.js
+++ 01_Fundamentos/bloco-04-introducao-javaScript-logica-programacao/04-objetos-funções/scripts.js
@@ -14,20 +14,48 @@ info.recorrente = "Sim";
     console.log(info[valores]);
   } */
 
-let segundo = {
-  personagem: "Tio Patinhas",
-  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
-  nota: "O último MacPatinhas",
-  recorrente: "Sim",
-};
-function infoPersonagem(objeto1, objeto2) {
- for (let valor in objeto1) {
-     if (objeto1[valor] === 'Sim' && objeto2[valor] === 'Sim'){
-         return console.log ('Ambos recorrentes')
-     }
-    console.log(objeto1[valor] + ' e ' + objeto2[valor])
-    return ''
-  }
+// let segundo = {
+//   personagem: "Tio Patinhas",
+//   origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+//   nota: "O último MacPatinhas",
+//   recorrente: "Sim",
+// };
+// function infoPersonagem(objeto1, objeto2) {
+//  for (let valor in objeto1) {
+//      if (objeto1[valor] === 'Sim' && objeto2[valor] === 'Sim'){
+//          return console.log ('Ambos recorrentes')
+//      }
+//     console.log(objeto1[valor] + ' e ' + objeto2[valor])
+//     return ''
+//   }
   
-}
-console.log(infoPersonagem(info, segundo));
+// }
+// console.log(infoPersonagem(info, segundo));
+
+
+let leitor = {
+  nome: 'Julia',
+  sobrenome: 'Pessoa',
+  idade: 21,
+  livrosFavoritos: [
+    {
+      titulo: 'O Pior Dia de Todos',
+      autor: 'Daniela Kopsch',
+      editora: 'Tordesilhas',
+    },
+  ],
+};
+let descricaoLivro = leitor.livrosFavoritos[0].titulo
+console.log('O livro favorito de ' + leitor.nome + ' ' + leitor.sobrenome + ' se chama ' + descricaoLivro);
+
+leitor.livrosFavoritos.push (
+  {
+  titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+  autor: 'JK Rowling',
+  editora: 'Rocco',
+  }
+  )
+
+console.log(leitor);
+let tamanho = leitor.livrosFavoritos.length;
+console.log(leitor.nome, ' tem ', tamanho , ' livros favoritos');
