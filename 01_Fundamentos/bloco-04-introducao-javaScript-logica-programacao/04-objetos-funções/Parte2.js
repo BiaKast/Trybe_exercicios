2ee3719a4399158c6d97e08235150b8b11ad9803
BiaKast <biahdrew94@gmail.com>
2022-02-07 22:44:58 -0300

    questao 2

--- 01_Fundamentos/bloco-04-introducao-javaScript-logica-programacao/04-objetos-funções/Parte2.js
+++ 01_Fundamentos/bloco-04-introducao-javaScript-logica-programacao/04-objetos-funções/Parte2.js
@@ -1,13 +1,37 @@
-let palavra = 'arara';
-function verificacaoPalindromo(polindromo) {
-let verificar = '';
-    for (let index = 0; index < polindromo.length; index +=1){
-        verificar = polindromo[index] + verificar;
-    };
-    if (verificar === palavra){
-        return true;
-    } else {
-        return false;
+// let palavra = 'arara';
+// function verificacaoPalindromo(polindromo) {
+// let verificar = '';
+//     for (let index = 0; index < polindromo.length; index +=1){
+//         verificar = polindromo[index] + verificar;
+//     };
+//     if (verificar === palavra){
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+// console.log(verificacaoPalindromo(palavra));
+
+//Questao 2
+let arrayNumeros = [2, 3, 6, 7, 10, 1];
+function descobrirMaiorNumero(numeros) {
+    let maiorNumero = 0;
+    let indiceDoMaiorNumero = 0;
+    for (let index = 0; index < numeros.length; index +=1){
+        if(maiorNumero < numeros[index]){
+            maiorNumero = numeros[index]
+            if(maiorNumero){
+                indiceDoMaiorNumero = [index]
+            }
+        }
     }
+    return indiceDoMaiorNumero;
 }
-console.log(verificacaoPalindromo(palavra));
+console.log(descobrirMaiorNumero(arrayNumeros));
+// function indiceDoMaiorNumero(numero) {
+//     for (let index of numeros){
+
+//     }
+//     }
+// }
+// console.log(indiceDoMaiorNumero(descobrirMaiorNumero));
