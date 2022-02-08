808419aa65584b5b0ba9f7e59f49025041b7f126
BiaKast <biahdrew94@gmail.com>
2022-02-08 10:58:36 -0300

    questao 4

--- 01_Fundamentos/bloco-04-introducao-javaScript-logica-programacao/04-objetos-funções/Parte2.js
+++ 01_Fundamentos/bloco-04-introducao-javaScript-logica-programacao/04-objetos-funções/Parte2.js
@@ -13,25 +13,61 @@
 // console.log(verificacaoPalindromo(palavra));
 
 //Questao 2
-let arrayNumeros = [2, 3, 6, 7, 10, 1];
-function descobrirMaiorNumero(numeros) {
-    let maiorNumero = 0;
-    let indiceDoMaiorNumero = 0;
-    for (let index = 0; index < numeros.length; index +=1){
-        if(maiorNumero < numeros[index]){
-            maiorNumero = numeros[index]
-            if(maiorNumero){
-                indiceDoMaiorNumero = [index]
+// let arrayNumeros = [2, 3, 6, 7, 10, 1];
+// function descobrirMaiorNumero(numeros) {
+//     let maiorNumero = 0;
+//     let indiceDoMaiorNumero = 0;
+//     for (let index = 0; index < numeros.length; index +=1){
+//         if(maiorNumero < numeros[index]){
+//             maiorNumero = numeros[index]
+//             if(maiorNumero){
+//                 indiceDoMaiorNumero = [index]
+//             }
+//         }
+//     }
+//     return indiceDoMaiorNumero;
+// }
+// console.log(descobrirMaiorNumero(arrayNumeros));
+
+//Questao 3
+// let arrayNumeros = [2, 4, 6, 7, 10, 0, -3];
+// function descobrirMenorNumero(numeros) {
+//     let menorNumero = 0 ;
+//     let indiceDoMenorNumero = 0;
+//     for (let array = numeros[0]; array < numeros.length; array +=1){
+//         if(numeros[array] <= menorNumero){
+//             menorNumero = numeros[array]
+//         }
+//         if(menorNumero){
+//             indiceDoMenorNumero = [array]
+//          }
+//     }
+//     return indiceDoMenorNumero;
+// }
+// console.log(descobrirMenorNumero(arrayNumeros));
+let nomes = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
+function nomeComMaisCaracteres (array){
+    let palavra = '';
+    let verificar = 0;
+    let  resultado = '';
+    for(let index = 0; index < array.length; index += 1){
+        palavra = array[index];
+        let tamanhoPalavra = 0;
+        for (let index2 = 1; index2 <= palavra.length; index2 += 1){
+            tamanhoPalavra = [index2];
+            if (tamanhoPalavra > verificar){
+                verificar = tamanhoPalavra;
+                if(verificar){
+                    resultado = palavra;
+                }
             }
         }
     }
-    return indiceDoMaiorNumero;
+    // console.log(resultado);
+    return resultado;
 }
-console.log(descobrirMaiorNumero(arrayNumeros));
-// function indiceDoMaiorNumero(numero) {
-//     for (let index of numeros){
+console.log(nomeComMaisCaracteres(nomes));
 
-//     }
-//     }
+// function verificarTamanhoNome(string) {
 // }
-// console.log(indiceDoMaiorNumero(descobrirMaiorNumero));
+// console.log(verificarTamanhoNome(palavra));
