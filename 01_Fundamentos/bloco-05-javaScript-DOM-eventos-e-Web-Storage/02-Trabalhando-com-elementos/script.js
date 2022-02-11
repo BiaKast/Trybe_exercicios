bc92a2919f09b1d90d0c27850145effe331e8e47
BiaKast <biahdrew94@gmail.com>
2022-02-11 09:33:21 -0300

    questão 6

--- 01_Fundamentos/bloco-05-javaScript-DOM-eventos-e-Web-Storage/02-Trabalhando-com-elementos/script.js
+++ 01_Fundamentos/bloco-05-javaScript-DOM-eventos-e-Web-Storage/02-Trabalhando-com-elementos/script.js
@@ -12,19 +12,26 @@ function inserirNoBody(valor) {
 let body = document.getElementsByTagName("body")[0];
 console.log(inserirNoBody(body));
 
-//questão3 e 5
+//questão3 e 5 e 6
 function filhosDeMain(valor2) {
   for (let index = 0; index < valor2.length; index += 1) {
     let element = valor2[index];
-    let criando = document.createElement("section");
-    criando.className = "center-content";
-    element.appendChild(criando);
+
+    let criandoSection0 = document.createElement("section");
+    criandoSection0.className = "center-content";
+    element.appendChild(criandoSection0);
 
     if (index += 1) {
-      let criandoSection = document.createElement("section");
-      criandoSection.className = "left-content ";
-      element.appendChild(criandoSection);
+      let criandoSection1 = document.createElement("section");
+      criandoSection1.className = "left-content ";
+      element.appendChild(criandoSection1);
     }
+
+    if (index += 1) {
+        let criandoSection2 = document.createElement("section");
+        criandoSection2.className = "right-content";
+        element.appendChild(criandoSection2);
+      }
   }
 }
 let main = document.getElementsByClassName("main-content");
