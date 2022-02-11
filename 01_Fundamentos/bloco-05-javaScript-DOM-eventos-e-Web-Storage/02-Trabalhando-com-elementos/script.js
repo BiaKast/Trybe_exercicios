f646bb7cb95052a468bab463bfc857440af7d1e7
BiaKast <biahdrew94@gmail.com>
2022-02-11 10:15:19 -0300

    questão 8 e correção na questão 7

--- 01_Fundamentos/bloco-05-javaScript-DOM-eventos-e-Web-Storage/02-Trabalhando-com-elementos/script.js
+++ 01_Fundamentos/bloco-05-javaScript-DOM-eventos-e-Web-Storage/02-Trabalhando-com-elementos/script.js
@@ -21,17 +21,17 @@ function filhosDeMain(valor2) {
     criandoSection0.className = "center-content";
     element.appendChild(criandoSection0);
 
-    if (index += 1) {
+    if ((index += 1)) {
       let criandoSection1 = document.createElement("section");
       criandoSection1.className = "left-content";
       element.appendChild(criandoSection1);
     }
 
-    if (index += 1) {
-        let criandoSection2 = document.createElement("section");
-        criandoSection2.className = "right-content";
-        element.appendChild(criandoSection2);
-      }
+    if ((index += 1)) {
+      let criandoSection2 = document.createElement("section");
+      criandoSection2.className = "right-content";
+      element.appendChild(criandoSection2);
+    }
   }
 }
 let main = document.getElementsByClassName("main-content");
@@ -52,13 +52,41 @@ console.log(paragrafoSection(pText));
 
 //Questão 7
 function imgFilhoDeLefetContent(i) {
-    for (let index = 0; index < i.length; index += 1) {
-        let element = i[index];
-        
-        let criandoImg = document.createElement('img')
-        criandoImg.src = 'https://picsum.photos/200';
-        element.appendChild(criandoImg);
-    }
+  for (let index = 0; index < i.length; index += 1) {
+    let element = i[index];
+
+    let criandoImg = document.createElement("img");
+    criandoImg.src = "https://picsum.photos/200";
+    criandoImg.className = "small-image";
+    element.appendChild(criandoImg);
+  }
 }
-let section1 = document.getElementsByClassName('left-content');
+let section1 = document.getElementsByClassName("left-content");
 console.log(imgFilhoDeLefetContent(section1));
+
+//Questão 8
+let list = [
+  "um",
+  "dois",
+  "três",
+  "quatro",
+  "cinco",
+  "seis",
+  "sete",
+  "oito",
+  "nove",
+  "dez",
+];
+function filhoDeRightContent(section, ii) {
+  let criandoList = document.createElement("ul");
+  section.appendChild(criandoList);
+
+  for (let index = 0; index < ii.length; index += 1) {
+    let element = ii[index];
+    let criandoFilhosList = document.createElement("li");
+    criandoFilhosList.innerText = element
+    criandoList.appendChild(criandoFilhosList);
+  }
+}
+let section2 = document.getElementsByClassName("right-content")[0];
+filhoDeRightContent(section2, list);
