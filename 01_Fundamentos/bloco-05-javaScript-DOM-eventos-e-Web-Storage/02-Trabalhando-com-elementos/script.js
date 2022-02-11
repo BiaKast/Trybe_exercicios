44778c4cd18be9197380bec3f1b610fcd1851657
BiaKast <biahdrew94@gmail.com>
2022-02-11 09:29:38 -0300

    questão 5

--- 01_Fundamentos/bloco-05-javaScript-DOM-eventos-e-Web-Storage/02-Trabalhando-com-elementos/script.js
+++ 01_Fundamentos/bloco-05-javaScript-DOM-eventos-e-Web-Storage/02-Trabalhando-com-elementos/script.js
@@ -1,40 +1,46 @@
 function inserirNoBody(valor) {
   let novaTag = document.createElement("h1");
   novaTag.innerText = "Exercício 5.2 - JavaScript DOM";
- 
+
   valor.appendChild(novaTag);
 
   let segundaTag = document.createElement("main");
   segundaTag.className = "main-content";
-  
+
   valor.appendChild(segundaTag);
 }
 let body = document.getElementsByTagName("body")[0];
 console.log(inserirNoBody(body));
 
-//questão3
-function inserirMain(valor2) {
+//questão3 e 5
+function filhosDeMain(valor2) {
   for (let index = 0; index < valor2.length; index += 1) {
     let element = valor2[index];
-    let creatMain = document.createElement("section");
-    creatMain.className = "center-content";
+    let criando = document.createElement("section");
+    criando.className = "center-content";
+    element.appendChild(criando);
 
-    element.appendChild(creatMain);
+    if (index += 1) {
+      let criandoSection = document.createElement("section");
+      criandoSection.className = "left-content ";
+      element.appendChild(criandoSection);
+    }
   }
-  return document.getElementsByClassName("center-content");
 }
 let main = document.getElementsByClassName("main-content");
-console.log(inserirMain(main));
+console.log(filhosDeMain(main));
 
 //questão 4
 function paragrafoSection(p) {
-    for (let index = 0; index < p.length; index += 1) {
-        let element = p[index];
-        let creatParagrafo = document.createElement('p');
-        creatParagrafo.innerText = 'adineretion dendi won a cant dragadu.'
+  for (let index = 0; index < p.length; index += 1) {
+    let element = p[index];
+    let creatParagrafo = document.createElement("p");
+    creatParagrafo.innerText = "Adineretion dendi won a cant dragadu.";
 
-        element.appendChild(creatParagrafo);
-    }
+    element.appendChild(creatParagrafo);
+  }
 }
 let pText = document.getElementsByClassName("center-content");
 console.log(paragrafoSection(pText));
+
+
