dc6b2f14b47a0547ebf9edcf9bdac354c15ae8e8
BiaKast <biahdrew94@gmail.com>
2022-02-10 23:11:53 -0300

    questão 3

--- 01_Fundamentos/bloco-05-javaScript-DOM-eventos-e-Web-Storage/02-Trabalhando-com-elementos/script.js
+++ 01_Fundamentos/bloco-05-javaScript-DOM-eventos-e-Web-Storage/02-Trabalhando-com-elementos/script.js
@@ -1,11 +1,26 @@
-let body = document.getElementsByTagName("body")[0];
 function inserirBody(valor) {
   let novaTag = document.createElement("h1");
   novaTag.innerText = "Exercício 5.2 - JavaScript DOM";
-    valor.appendChild(novaTag)
+ 
+  valor.appendChild(novaTag);
 
-    let segundaTag = document.createElement("main");
-    segundaTag.className = 'main-content';
-    valor.appendChild(segundaTag);
+  let segundaTag = document.createElement("main");
+  segundaTag.className = "main-content";
+  
+  valor.appendChild(segundaTag);
 }
+let body = document.getElementsByTagName("body")[0];
 console.log(inserirBody(body));
+
+//questão3
+function inserirMain(valor2) {
+  for (let index = 0; index < valor2.length; index += 1) {
+    let element = valor2[index];
+    let creatMain = document.createElement("section");
+    creatMain.className = "center-content";
+
+    element.appendChild(creatMain);
+  }
+}
+let main = document.getElementsByClassName("main-content");
+console.log(inserirMain(main));
