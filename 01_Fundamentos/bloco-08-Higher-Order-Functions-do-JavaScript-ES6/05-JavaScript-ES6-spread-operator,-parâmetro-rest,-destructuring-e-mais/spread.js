75490a5e3f7d9c07256229d6622e54fecfdb2861
BiaKast <biahdrew94@gmail.com>
2022-03-15 18:58:10 -0300

    usando spread

--- 01_Fundamentos/bloco-08-Higher-Order-Functions-do-JavaScript-ES6/05-JavaScript-ES6-spread-operator,-parâmetro-rest,-destructuring-e-mais/spread.js
+++ 01_Fundamentos/bloco-08-Higher-Order-Functions-do-JavaScript-ES6/05-JavaScript-ES6-spread-operator,-parâmetro-rest,-destructuring-e-mais/spread.js
@@ -1,3 +1,4 @@
+// 1 - Dado o código abaixo, complete-o de forma que seja impressa a área dos 3 retângulos. O código deve retornar em sequência 2 , 15 e 54 .
 const rectangleArea = (width, height) => width * height;
 
 const rectangle1 = [1, 2];
